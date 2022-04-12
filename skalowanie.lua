7aec09c5b56f9f505547ada94b723b9a7d80c3cd
Sylwek <74017399+Sylwegs@users.noreply.github.com>
2022-04-12 13:43:14 +0200

    poprawa

--- skalowanie.lua
+++ skalowanie.lua
@@ -1,9 +1,8 @@
 local sx, sy = guiGetScreenSize()
-zmienna = 1920/sx
+zoom = 1920/sx
 
 function zlaomasz()
-	dxDrawText("Wymiary twojej roździelczości są bliższe kwadratowi niż prostokątowi.\nZalecana roździelczość: 1920 x 1080, 1600x900, 1280x720\nNa twojej roździelczości niektóre elementy gui mogą nie działać poprawnie!\nAby wyłączyć tą wiadomość wpisz /zrozumialem", 11/zmienna, 11/zmienna, 1911/zmienna, 72/zmienna, tocolor(0, 0, 0, 255), 1.2/zmienna, "default-bold", "center", "center", false, false, false, false, false)
-	dxDrawText("Wymiary twojej roździelczości są bliższe kwadratowi niż prostokątowi.\nZalecana roździelczość: 1920 x 1080, 1600x900, 1280x720\nNa twojej roździelczości niektóre elementy gui mogą nie działać poprawnie!\nAby wyłączyć tą wiadomość wpisz /zrozumialem", 10/zmienna, 10/zmienna, 1910/zmienna, 71/zmienna, tocolor(255, 0, 0, 255), 1.2/zmienna, "default-bold", "center", "center", false, false, false, false, false)
+	dxDrawText("Wymiary twojej roździelczości są bliższe kwadratowi niż prostokątowi.\nZalecana roździelczość: 1920 x 1080, 1600x900, 1280x720\nNa twojej roździelczości niektóre elementy gui mogą nie działać poprawnie!\nAby wyłączyć tą wiadomość wpisz /zrozumialem", 10/zoom, 10/zoom, 1910/zoom, 71/zoom, tocolor(255, 255, 255, 255), 1, "default-bold", "center", "center", false, false, false, false, false)
 end
 
 function sprawdzstart() 
